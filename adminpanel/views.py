cf53edd826e0dd0553e28feed2d030645de0106b
Alvine Clive Munene <alvine.munene@student.moringaschool.com>
2025-07-17 19:06:49 +0300

    admin backend for booked

---
+++ adminpanel/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Book, Order, LendingRequest
+from .serializers import BookSerializer, OrderSerializer, LendingRequestSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class LendingRequestViewSet(viewsets.ModelViewSet):
+    queryset = LendingRequest.objects.all()
+    serializer_class = LendingRequestSerializer
