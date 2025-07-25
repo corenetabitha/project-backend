4fa7bbd208a7cbf79bd463d6595cd4c54bc17eb8
Alvine Clive Munene <alvine.munene@student.moringaschool.com>
2025-07-25 19:08:53 +0300

    edited adminpanel

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework import viewsets
 from .models import Book, Order, LendingRequest
 from .serializers import BookSerializer, OrderSerializer, LendingRequestSerializer
